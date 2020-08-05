c3eed4b0eedbd30ee1ff3b9f9992bea9c0bf5b22
李进辉 <jinhui@JINHUI.halfroad.com>
2020-08-05 17:58:14 +0800

    1. Added multiple languages support. 2. Added the class to read/write health data

---
+++ SportsTracer/Managers/HealthDataManager.swift
@@ -0,0 +1,28 @@
+//
+//  HealthDataManager.swift
+//  SportsTracer
+//
+//  Created by 李进辉 on 2020/8/5.
+//  Copyright © 2020 HalfRoad Software Inc. All rights reserved.
+//
+
+import UIKit
+import HealthKit
+
+class HealthDataManager: NSObject {
+
+    func acquire() {
+        
+        let allTypes = Set([HKObjectType.workoutType(),
+                            HKObjectType.quantityType(forIdentifier: .activeEnergyBurned)!,
+                            HKObjectType.quantityType(forIdentifier: .distanceCycling)!,
+                            HKObjectType.quantityType(forIdentifier: .distanceWalkingRunning)!,
+                            HKObjectType.quantityType(forIdentifier: .heartRate)!])
+        let healthStore = HKHealthStore()
+        healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
+            if !success {
+                // Handle the error here.
+            }
+        }
+    }
+}
